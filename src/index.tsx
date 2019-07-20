494c7bb9f6b8c83203e3a6ba558e08d5b5806b97
Per Høgfeldt <hogfeldt38@gmail.com>
2019-07-20 16:57:00 +0200

    First iteration of a plan generator function

--- src/index.tsx
+++ src/index.tsx
@@ -39,8 +39,37 @@ class AppComponent extends React.Component<{}, {}> {
             <ControlPanel />
           </div>
         </div>
+        <button onClick={_e => this.generatePlan()}>
+          Generer Barplan
+        </button>
       </>
     );
+  }
+
+  @action
+  generatePlan() {
+    for (let t=0 ; t<app.timeNames.length ; t++) {
+      for (let l=0 ; l<app.locationNames.length ; l++) {
+        if (app.locationNames[l] === "Drinksbar") {
+          this.fillSlot(t,l,6)
+        } else if (app.locationNames[l] === "Kamera") {
+          this.fillSlot(t,l,1)
+        } else {
+          this.fillSlot(t,l,3)
+        }
+      }
+    }
+  }
+
+  fillSlot(time: number, location: number, amount: number) {
+    let workers = 0;
+    while (workers<amount) {
+      const randNum = Math.floor(Math.random() * app.persons.length)
+      if (!app.personsWorkslots[randNum].some(slot => slot[0] == time)) {
+        app.personsWorkslots[randNum].push([time, location, false]);
+        workers++;
+      }
+    }
   }
 
   @action
